9f56353e30ef9733f879f985ec23379af89b3a77
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-10-22 19:04:35 +0900

    ADDING onclick

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
 export default class Counter extends React.Component {
+  handleAttck() {
+    alert("Attack clicked");
+  }
+
+  handleDefend() {
+    alert("Defend clicked");
+  }
   render() {
     return (
       <div className="row text-black">
         <h1>Counter:</h1>
-        <button style={{ width: "200px" }}>+1</button>
-        <button style={{ width: "200px" }}>-1</button>
+        <button onClick={this.handleAttck} style={{ width: "200px" }}>
+          +1
+        </button>
+        <button onClick={this.handleDefend} style={{ width: "200px" }}>
+          -1
+        </button>
       </div>
     );
   }
